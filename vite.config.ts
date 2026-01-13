455817263c5850040cd25cce8d0ae88582f8a221
Kateryna <ekaterina.melnik00@gmail.com>
2026-01-13 12:20:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // Цей рядок критично важливий для GitHub Pages
-    base: '-1.0./', 
+    base: '-1.0/', 
     server: {
       port: 3000,
       host: '0.0.0.0',
